b60eba7507fd20cacf9dbce7805f1d71c7c7b481
dpinnamreddy <dilip.pinnamreddy@gmail.com>
2022-10-01 04:29:29 +0000

    first commit

---
+++ src/components/CowinDashboard/index.js
@@ -0,0 +1,77 @@
+// Write your code here
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import VaccinationCoverage from '../VaccinationCoverage'
+import VaccinationByGender from '../VaccinationByGender'
+import VaccinationByAge from '../VaccinationByAge'
+import './index.css'
+
+class CowinDashboard extends Component {
+  state = {
+    data: '',
+    isSuccess: true,
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    this.getCowinData()
+  }
+
+  getCowinData = async () => {
+    const vaccinationDataApiUrl = 'https://apis.ccbp.in/covid-vaccination-data'
+    const options = {
+      method: 'GET',
+    }
+    const response = await fetch(vaccinationDataApiUrl, options)
+    if (response.ok) {
+      const data = await response.json()
+      const updatedData = {
+        last7DaysVaccination: data.last_7_days_vaccination,
+        vaccinationByAge: data.vaccination_by_age,
+        vaccinationByGender: data.vaccination_by_gender,
+      }
+      this.setState({data: updatedData, isSuccess: true, isLoading: false})
+    } else {
+      this.setState({isSuccess: false, isLoading: false})
+    }
+  }
+
+  render() {
+    const {data, isSuccess, isLoading} = this.state
+    return (
+      <div className="container">
+        {isLoading ? (
+          <div className="loader" testid="loader">
+            <Loader />
+          </div>
+        ) : (
+          <>
+            <div className="logo">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/cowin-logo.png"
+                alt="website logo"
+                className="logo-img"
+              />
+              <p>Co-WIN</p>
+            </div>
+            <h1>CoWIN Vaccination in India</h1>
+            {isSuccess ? (
+              <>
+                <VaccinationCoverage details={data.last7DaysVaccination} />
+                <VaccinationByGender details={data.vaccinationByAge} />
+                <VaccinationByAge details={data.vaccinationByGender} />
+              </>
+            ) : (
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/api-failure-view.png"
+                alt="failure view"
+              />
+            )}
+          </>
+        )}
+      </div>
+    )
+  }
+}
+
+export default CowinDashboard
---
+++ src/components/VaccinationByAge/index.js
@@ -0,0 +1,37 @@
+// Write your code here
+import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+import './index.css'
+
+const VaccinationByAge = props => {
+  const {details} = props
+  return (
+    <>
+      <h1>Vaccination by Age</h1>
+      <ResponsiveContainer width="100%" height={300}>
+        <PieChart>
+          <Pie
+            cx="70%"
+            cy="40%"
+            data={details}
+            startAngle={0}
+            endAngle={360}
+            outerRadius="70%"
+            dataKey="count"
+          >
+            <Cell name="18-44" fill="#fecba6" />
+            <Cell name="44-60" fill="#b3d23f" />
+            <Cell name="Others" fill="#a44c9e" />
+          </Pie>
+          <Legend
+            iconType="circle"
+            layout="vertical"
+            verticalAlign="middle"
+            align="right"
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </>
+  )
+}
+
+export default VaccinationByAge
